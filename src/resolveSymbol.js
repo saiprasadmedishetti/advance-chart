e3c22554754777dd09b8b2bea241abb09027b92a
Saiprasad Medishetti <saiprasadmedishetti@Saiprasads-MacBook-Air.local>
2025-08-23 16:16:40 +0530

    first commit

---
+++ src/resolveSymbol.js
@@ -0,0 +1,33 @@
+export const resolveSymbol = (
+  symbolName,
+  onSymbolResolvedCallback,
+  onResolveErrorCallback,
+  extension
+) => {
+  const tokenSymbol = "BTC";
+
+  if (!tokenSymbol) {
+    onResolveErrorCallback();
+  } else {
+    const symbolInfo = {
+      ticker: tokenSymbol,
+      name: `${tokenSymbol}/USDT`,
+      session: "24x7",
+      timezone: "Etc/UTC",
+      minmov: 1,
+      // pricescale: 10000000,
+      pricescale: 1000,
+      has_intraday: true,
+      intraday_multipliers: ["1", "5", "15", "30", "60"],
+      has_empty_bars: false,
+      has_weekly_and_monthly: false,
+      supported_resolutions: ["1", "5", "15", "30", "60", "1D", "1W", "1M"],
+      supported_intervals: ["1", "5", "15", "30", "60", "1D", "1W", "1M"],
+      // supported_resolutions: ['1D'],
+      volume_precision: 1,
+      data_status: "streaming",
+      countBack: 30,
+    };
+    onSymbolResolvedCallback(symbolInfo);
+  }
+};
